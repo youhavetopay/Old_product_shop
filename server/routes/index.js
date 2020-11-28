49de99d01c0efca9d57b8d53f25934f68fd0f0d6
moon <yunii23@naver.com>
2020-11-28 20:11:38 +0900

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const mainController = require("../controller/index.js")
 const main = new mainController();
 
 router.get('/', function (req, res, next) {
-    res.render('index.ejs',{title: '아이즈원', sess : req.session});
+    res.render('index.ejs',{title: '아이즈원', sess : req.session.user_id});
   });
 
 
