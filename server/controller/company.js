23e2db9d62988e4155f6e1575ac496239246a59d
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-11-30 16:48:05 +0900

    주석달기

--- server/controller/company.js
+++ server/controller/company.js
@@ -1,10 +1,14 @@
 const pool = require("../dbconfig/dbconfig")
 
 class companyController {  
+
+    // 
     async selectCompany (req, res, next) {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 공급업체인지 확인하기???
                 const ynSql = `SELECT * FROM company WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(ynSql, (err, yn) => {
@@ -14,7 +18,10 @@ class companyController {
                     else {
                         console.log(yn[0].company_num);
 
+                        // 등록된 상품 갯수 들고 오기
                         const productCountSql = `SELECT COUNT(*) FROM product WHERE company_num = "${yn[0].company_num}"`
+
+                        // 내가 등록한 상품을 주문한 개수
                         const orderCountSql = `SELECT COUNT(o.order_num) FROM orderinfo WHERE {SELECT product_num FROM prodcut WHERE p.company_num = "${yn[0].company_num}}"`
                     } 
                 })
--- server/controller/users.js
+++ server/controller/users.js
@@ -32,9 +32,11 @@ class userController {
             if(err) throw res.json({success: false, err});
             else{
                 
+                // 지역정보 가져오기
                 const areaSql = `SELECT * FROM area WHERE area_num = "${req.body.area_num}"`
                 console.log(req.params.area_num);
                
+                // 회원가입 정보 넣기
                 var sql = "INSERT INTO users VALUES (?,?,?,?,?,?)";
 
                 if (req.body.user_id == '' || req.body.user_pw == '' || req.body.user_name == '' || req.body.user_tel == '' || req.body.area_id == '') {
@@ -71,6 +73,7 @@ class userController {
         pool.getConnection((err, conn) => {
             if(err) throw err;
             else{
+                
                 var sql = `SELECT * FROM users WHERE user_id = "${req.body.user_id}" AND user_pw = "${req.body.user_pw}"`;
 
                 conn.query(sql, (err, row)=>{
@@ -98,26 +101,41 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 사용자가 가지고 있는 쿠폰가져오기
                 const couponSql = `SELECT * FROM coupon WHERE user_id = "${req.session.user_id}"`
+
+                // 배송중인 주문 수 가져오기
                 const orderStateSql = `SELECT COUNT(order_state) FROM orders WHERE user_id = "${req.session.user_id}" AND order_state = "배송중"`
+
+                // 직거래 예정인 주문 수 가져오기
                 const directSql = `SELECT COUNT(order_direct_whether) FROM orders WHERE user_id = "${req.session.user_id}" AND order_direct_whether = "Y"`
+
+                // 내가 주문한 주문 목록 가져오기
                 const MyOrderListSql = `SELECT * FROM orders WHERE user_id = "${req.session.user_id}"`
 
+
+                // 사용자가 가지고 있는 쿠폰가져오기
                 conn.query(couponSql, (err, coupon) => {
                     console.log("에러1");
                     if (err) throw err;
                     else {
                         
+
+                        // 배송중인 주문 수 가져오기
                         conn.query(orderStateSql, (err, orderstate) => {
                             console.log("에러2");
                             if (err) throw err;
                             else {
 
+
+                                // 직거래 예정인 주문 수 가져오기
                                 conn.query(directSql, (err, direct) => {
                                     console.log("에러3");
                                     if (err) throw err;
                                     else {
 
+                                        // 내가 주문한 주문 목록 가져오기
                                         conn.query(MyOrderListSql, (err, myorderlist) => {
                                             console.log("에러4");
                                             if (err) throw err;
@@ -148,6 +166,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                //사용자가 가지고 있는 카드 가져오기
                 const sql = `SELECT * FROM card WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err,row)=> {
@@ -168,6 +188,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 카드 새로 추가하기
                 const sql = `INSERT INTO card VALUES (?,?,?,?)`
                 const val = [req.body.card_num, req.body.card_validity, req.body.card_cvc, req.session.user_id]
 
@@ -186,6 +208,8 @@ class userController {
     async deleteCard (req, res, next) {
         pool.getConnection((err, conn)=>{
             if(err) throw err;
+
+            // 카드 삭제하기
             const sql = `DELETE FROM card WHERE card_num = "${req.params.card_num}"`
             conn.query(sql, (err, row)=>{
                 conn.release();
@@ -205,6 +229,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 배송지 가져오기
                 const sql = `SELECT * FROM place WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err,row)=> {
@@ -225,6 +251,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 배송지 추가하기
                 const sql = `INSERT INTO place(?,?,?,?,?,?,?) VALUES (?,?,?,?,?,?,?)`
                 const val = [req.body.place_num, req.body.place_addr, req.body.place_addrinfo, req.body.place_name, req.body.place_userNM, req.body.place_tel, req.session.user_id]
 
@@ -243,6 +271,8 @@ class userController {
       async deletePlace (req, res, next) {
         pool.getConnection((err, conn)=>{
             if(err) throw err;
+
+            // 배송지 삭제하기
             const sql = `DELETE FROM place WHERE place_id = "${req.params.place_id}"`
             conn.query(sql, (err, row)=>{
                 conn.release();
@@ -262,6 +292,7 @@ class userController {
 
             const place = req.body;
 
+            // 배송지 수정하기
             const sql = `UPDATE place(?,?,?,?,?,?) SET (?,?,?,?,?,?)  WHERE place_id = "${req.params.place_id}"`;
             const val = [req.body.place_num, req.body.place_addr, req.body.place_addrinfo, req.body.place_name, req.body.place_userNM, req.body.place_tel];
 
@@ -286,6 +317,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 즐겨찾기 목록 가져오기
                 const sql = `SELECT * FROM bookmark WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err, row) => {
@@ -306,6 +339,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 즐겨찾기 삭제하기
                 const sql = `DELETE FROM bookmark WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err, row) => {
@@ -325,6 +360,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 장바구니 가져오기???
                 const sql = `SELECT * FROM basket WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err, row) => {
@@ -345,6 +382,8 @@ class userController {
         pool.getConnection((err, conn) => {
             if (err) throw err;
             else {
+
+                // 사용자가 가지고 있는 쿠폰 가져오기
                 const sql = `SELECT * FROM coupon WHERE user_id = "${req.session.user_id}"`
 
                 conn.query(sql, (err, row) => {
