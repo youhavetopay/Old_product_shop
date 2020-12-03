b693caf5526acc3c50514a9058debc8efef75b4e
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-12-04 00:18:42 +0900

    주문 만드는 중

--- server/controller/order.js
+++ server/controller/order.js
@@ -2,48 +2,70 @@ const pool = require("../dbconfig/dbconfig")
 
 class orderController {
 
-    async getOrderProduct(req, res, next){
-        pool.getConnection((err, conn)=>{
-            if(err) throw err;
+    async getOrderProduct(req, res, next) {
+        pool.getConnection((err, conn) => {
+            if (err) throw err;
 
-            if(req.body.product_num.length == 1){
-                conn.query('select * from product, image where image_seq = 1 and product_num = ? and product_num = fk_product_num',[
+            if (req.body.product_num.length == 1) {
+                conn.query('select * from product, image where image_seq = 1 and product_num = ? and product_num = fk_product_num', [
                     req.body.product_num
-                ], (err, order_product_list)=>{
-                    if(err) throw err;
+                ], (err, order_product_list) => {
+                    if (err) throw err;
 
-                    conn.query('SELECT * FROM place, card where card.user_id = ? and place.user_id = ?;',[
-                        req.session.user_id, req.session.user_id
-                    ], (err, user_info)=>{
-                        if(err) throw err;
+                    conn.query('select * from place where user_id = ?', [
+                        req.session.user_id
+                    ], (err, user_address) => {
+                        if (err) throw err;
 
-                        req.order_product_list = order_product_list;
-                        req.user_info = user_info;
+                        conn.query('select * from card where user_id = ?', [
+                            req.session.user_id
+                        ], (err, user_card) => {
+                            if (err) throw err;
 
-                        conn.release();
-                        next();
+
+                            req.order_product_list = order_product_list;
+                            req.user_address = user_address;
+                            req.user_card = user_card;
+
+                            req.total_money = order_product_list[0].product_price * req.body.order_count
+
+                            conn.release();
+                            next();
+
+                        })
                     })
+
                 })
-            }
-            else{
+            } else {
                 conn.query(`SELECT p.*, i.image_content, bi.bakset_sum
                 from product p, image i, baksetinfo bi, bakset b
                 WHERE p.product_num = i.fk_product_num AND i.image_seq = 1 AND p.product_num = bi.product_num AND b.user_id = ?
-                `,[
+                `, [
                     req.session.user_id
-                ], (err, order_product_list)=>{
-                    if(err) throw err;
+                ], (err, order_product_list) => {
+                    if (err) throw err;
 
-                    conn.query('SELECT * FROM place, card where card.user_id = ? and place.user_id = ?;',[
-                        req.session.user_id, req.session.user_id
-                    ], (err, user_info)=>{
-                        if(err) throw err;
+                    conn.query('select * from place where user_id = ?', [
+                        req.session.user_id
+                    ], (err, user_address) => {
+                        if (err) throw err;
 
-                        req.order_product_list = order_product_list;
-                        req.user_info = user_info;
+                        conn.query('select * from card where user_id = ?', [
+                            req.session.user_id
+                        ], (err, user_card) => {
+                            if (err) throw err;
 
-                        conn.release();
-                        next();
+
+                            req.order_product_list = order_product_list;
+                            req.user_address = user_address;
+                            req.user_card = user_card;
+
+                            req.total_money = order_product_list[0].product_price * req.body.order_count
+
+                            conn.release();
+                            next();
+
+                        })
                     })
                 })
             }
--- server/routes/order.js
+++ server/routes/order.js
@@ -5,7 +5,17 @@ const order = new orderController();
 
 
 router.post('/', order.getOrderProduct,(req, res, next)=>{
-    res.render('order/order', {sess: req.session.user_id, productList:req.order_product_list, userInfo:req.user_info})
+
+    console.log(req.totalMoney);
+
+    res.render('order/order', {
+        sess: req.session.user_id, 
+        productList:req.order_product_list, 
+        user_address: req.user_address,
+        user_card:req.user_card, 
+        count:req.body.order_count,
+        totalMoney: req.total_money
+    })
 })
 
 module.exports = router;
