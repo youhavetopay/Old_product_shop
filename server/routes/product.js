8a1583390e352c96789eda7d7c7556a2484cf8cc
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-12-07 23:00:19 +0900

    즐겨찾기 추가

--- server/routes/product.js
+++ server/routes/product.js
@@ -12,6 +12,11 @@ router.get('/:product_num', Product.getProductDetail, (req, res, next)=>{
         reviewList: req.reviewList});
 })
 
-
+router.post('/addBookMark', Product.addBookMark, (req, res, next)=>{
+    res.send(`<script type="text/javascript">
+            alert("해당 업체가 즐겨찾기에 추가되었습니다.."); 
+            history.back();
+            </script>`)
+})
 
 module.exports = router;
