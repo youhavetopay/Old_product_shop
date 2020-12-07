ac2f5788c1403f5b0e3158590fa1672841ec4d8f
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-12-07 23:58:33 +0900

    바로주문 수정

--- server/routes/order.js
+++ server/routes/order.js
@@ -14,7 +14,7 @@ router.post('/', order.getOrderProduct,(req, res, next)=>{
         productList:req.order_product_list, 
         user_address: req.user_address,
         user_card:req.user_card, 
-        count:req.body.order_count,
+        count:req.body.product_count,
         totalMoney: req.total_money,
         check_basket: req.basket_check
     })
